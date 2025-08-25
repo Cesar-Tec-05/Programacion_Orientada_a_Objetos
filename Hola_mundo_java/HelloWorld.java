a68af5b698750d1015ed32ddbfd30cd8a62ebc2d
Cesar de Jesus Becerra Vera <cesardejesusbecerravera@gmail.com>
2025-08-25 15:57:53 -0600

    Hola Mundo Java

---
+++ Hola_mundo_java/HelloWorld.java
@@ -0,0 +1,14 @@
+package Hola_mundo_java;
+// CESAR DE JESUS BECERRA VERA 
+// PROGRAMACION ORIENTADA A OBJETOS
+// 25 AGOSTO DE 2025
+// DESCRIPCION: PROGRAMA QUE IMPRIME "HOLA MUNDO"
+/* Clase se debe llamar igual que el archivo */
+public class HelloWorld {
+    /** 
+        Este comentario es de documentación.
+    */
+    public static void main(String[] args) { // inicio de la función main
+        System.out.println("Hola, Mundo!");
+    } // fin de la función main
+} //  fin de la clase 
